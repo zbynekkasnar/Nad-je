bef2539a9f737e0ffe0953d11c1e6160ff292be5
sazkynadeje <zbynek.kasnar@gmail.com>
2026-02-18 20:07:12 +0100

    Create stats.js

---
+++ stats.js
@@ -0,0 +1,46 @@
+const vsechnyStaty = [
+    // EVROPA
+    { n: "Česko", c: "cz" }, { n: "Slovensko", c: "sk" }, { n: "Německo", c: "de" },
+    { n: "Rakousko", c: "at" }, { n: "Švýcarsko", c: "ch" }, { n: "Francie", c: "fr" },
+    { n: "Itálie", c: "it" }, { n: "Španělsko", c: "es" }, { n: "Portugalsko", c: "pt" },
+    { n: "Belgie", c: "be" }, { n: "Nizozemsko", c: "nl" }, { n: "Velká Británie", c: "gb" },
+    { n: "Anglie", c: "gb-eng" }, { n: "Skotsko", c: "gb-sct" }, { n: "Wales", c: "gb-wls" },
+    { n: "Severní Irsko", c: "gb-nir" }, { n: "Dánsko", c: "dk" }, { n: "Finsko", c: "fi" },
+    { n: "Švédsko", c: "se" }, { n: "Norsko", c: "no" }, { n: "Island", c: "is" },
+    { n: "Polsko", c: "pl" }, { n: "Maďarsko", c: "hu" }, { n: "Slovinsko", c: "si" },
+    { n: "Chorvatsko", c: "hr" }, { n: "Srbsko", c: "rs" }, { n: "Bosna a Hercegovina", c: "ba" },
+    { n: "Černá Hora", c: "me" }, { n: "Severní Makedonie", c: "mk" }, { n: "Albánie", c: "al" },
+    { n: "Řecko", c: "gr" }, { n: "Turecko", c: "tr" }, { n: "Rumunsko", c: "ro" },
+    { n: "Bulharsko", c: "bg" }, { n: "Ukrajina", c: "ua" }, { n: "Bělorusko", c: "by" },
+    { n: "Rusko", c: "ru" }, { n: "Lotyšsko", c: "lv" }, { n: "Litva", c: "lt" },
+    { n: "Estonsko", c: "ee" }, { n: "Gruzie", c: "ge" }, { n: "Arménie", c: "am" },
+    { n: "Ázerbájdžán", c: "az" }, { n: "Kazachstán", c: "kz" }, { n: "Izrael", c: "il" },
+    
+    // SEVERNÍ A STŘEDNÍ AMERIKA
+    { n: "Kanada", c: "ca" }, { n: "USA", c: "us" }, { n: "Mexiko", c: "mx" },
+    { n: "Kostarika", c: "cr" }, { n: "Panama", c: "pa" }, { n: "Honduras", c: "hn" },
+    { n: "Jamajka", c: "jm" }, { n: "Kuba", c: "cu" }, { n: "Salvador", c: "sv" },
+    
+    // JIŽNÍ AMERIKA
+    { n: "Brazílie", c: "br" }, { n: "Argentina", c: "ar" }, { n: "Uruguay", c: "uy" },
+    { n: "Chile", c: "cl" }, { n: "Kolumbie", c: "co" }, { n: "Ekvádor", c: "ec" },
+    { n: "Peru", c: "pe" }, { n: "Paraguay", c: "py" }, { n: "Venezuela", c: "ve" },
+    { n: "Bolívie", c: "bo" },
+    
+    // ASIE
+    { n: "Japonsko", c: "jp" }, { n: "Jižní Korea", c: "kr" }, { n: "Čína", c: "cn" },
+    { n: "Severní Korea", c: "kp" }, { n: "Saúdská Arábie", c: "sa" }, { n: "Írán", c: "ir" },
+    { n: "Irák", c: "iq" }, { n: "Katar", c: "qa" }, { n: "SAE", c: "ae" },
+    { n: "Thajsko", c: "th" }, { n: "Vietnam", c: "vn" }, { n: "Indonésie", c: "id" },
+    { n: "Uzbekistán", c: "uz" }, { n: "Indie", c: "in" }, { n: "Tchaj-wan", c: "tw" },
+    
+    // AFRIKA
+    { n: "Egypt", c: "eg" }, { n: "Maroko", c: "ma" }, { n: "Tunisko", c: "tn" },
+    { n: "Alžírsko", c: "dz" }, { n: "Senegal", c: "sn" }, { n: "Nigérie", c: "ng" },
+    { n: "Kamerun", c: "cm" }, { n: "Ghana", c: "gh" }, { n: "Pobřeží slonoviny", c: "ci" },
+    { n: "JAR", c: "za" }, { n: "Mali", c: "ml" }, { n: "DR Kongo", c: "cd" },
+    { n: "Angola", c: "ao" }, { n: "Zambie", c: "zm" }, { n: "Togo", c: "tg" },
+    
+    // OCEÁNIE
+    { n: "Austrálie", c: "au" }, { n: "Nový Zéland", c: "nz" }
+];
